86396449b1d99c731c9f912e0e972f62b016dc22
RaneWallin <rane@ranewallin.com>
2019-05-25 08:45:50 -0700

    updating build again

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
     return (
         <footer>
-            <p>Designed and coded by Rane.</p>
+            <p>Designed and coded by <a href="https://blasph.me">Rane</a>.</p>
             <p><a href="https://github.com/RaneWallin/GameOfLife">View the Source.</a></p>
         </footer>
     );
